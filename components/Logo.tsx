5de0b4905c1e8af2c5170c5891cc52249c598f67
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-26 18:19:46 +0100

    Creating a dropdown menu component with client-side button trigger.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,11 +2,10 @@ import { SwitchCamera } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 
-const Logo = () => {
+function Logo() {
   return (
     <Link
       href={"/dashboard"}
-      //   buttonVariant, style an element that is not a button with buttons style.
       className={buttonVariants({
         className:
           "hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0",
@@ -15,8 +14,9 @@ const Logo = () => {
       })}
     >
       <SwitchCamera className="h-6 w-6 shrink-0 lg:hidden" />
-      <p className={`font-semibold text-xl hidden lg:block`}>PixelGram</p>
+      <p className={`font-semibold text-xl hidden lg:block `}>Pixelgram</p>
     </Link>
   );
-};
+}
+
 export default Logo;
