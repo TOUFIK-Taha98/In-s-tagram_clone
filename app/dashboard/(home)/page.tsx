d3f38f0607a9c20eccc0b60a3d1a3cfd5a3b58c4
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-27 10:29:53 +0100

    Preparing to use the suspense concept

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -1,5 +1,21 @@
+import Posts from "@/components/Posts";
+import { Suspense } from "react";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <main className="flex w-full flex-grow">
+      <div className="flex flex-col flex-1 gap-y-8 max-w-lg mx-auto pb-20">
+        {/* using suspense */}
+        <Suspense
+        // fallback={
+        // <PostsSkeleton />
+        // }
+        >
+          <Posts />
+        </Suspense>
+      </div>
+    </main>
+  );
 };
 
 export default DashboardPage;
---
+++ components/Posts.tsx
@@ -0,0 +1,6 @@
+const Posts = () => {
+  // TODO: Implement data fetching logic here...
+
+  return <>{/* Map through posts */}</>;
+};
+export default Posts;
