95d12e7d28c3f770491a38e745c470242dd0631b
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-27 11:13:18 +0100

    Form design with React Hook Form, Zod, and Sonner.

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -21,6 +21,7 @@ import {
 
 import { Input } from "@/components/ui/input";
 import useMount from "@/hooks/useMount";
+import { CreatePost } from "@/lib/schemas";
 // import useMount from "@/hooks/useMount";
 // import { createPost } from "@/lib/actions";
 // import { CreatePost } from "@/lib/schemas";
@@ -40,6 +41,17 @@ const CreatePage = () => {
   const router = useRouter();
 
   const mount = useMount();
+
+  const form = useForm<z.infer<typeof CreatePost>>({
+    resolver: zodResolver(CreatePost),
+    defaultValues: {
+      caption: "",
+      fileUrl: undefined,
+    },
+  });
+
+  const fileUrl = form.watch("fileUrl");
+
   if (!mount) return null;
 
   return (
@@ -52,6 +64,80 @@ const CreatePage = () => {
           <DialogHeader>
             <DialogTitle>Create new post</DialogTitle>
           </DialogHeader>
+
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(async (values) => {
+                console.log(values);
+              })}
+              className="space-y-4"
+            >
+              {!!fileUrl ? (
+                <div className="h-96 md:h-[450px] overflow-hidden rounded-md">
+                  <AspectRatio ratio={1 / 1} className="relative h-full">
+                    <Image
+                      src={fileUrl}
+                      alt="Post preview"
+                      fill
+                      className="rounded-md object-cover"
+                    />
+                  </AspectRatio>
+                </div>
+              ) : (
+                <FormField
+                  control={form.control}
+                  name="fileUrl"
+                  render={({ field, fieldState }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="picture">Picture</FormLabel>
+                      <FormControl>
+                        {/* <UploadButton
+                          endpoint="imageUploader"
+                          onClientUploadComplete={(res) => {
+                            form.setValue("fileUrl", res[0].url);
+                            toast.success("Upload complete");
+                          }}
+                          onUploadError={(error: Error) => {
+                            console.error(error);
+                            toast.error("Upload failed");
+                          }}
+                        /> */}
+                      </FormControl>
+                      <FormDescription>
+                        Upload a picture to post.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              )}
+
+              {!!fileUrl && (
+                <FormField
+                  control={form.control}
+                  name="caption"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel htmlFor="caption">Caption</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="caption"
+                          id="caption"
+                          placeholder="Write a caption..."
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              )}
+
+              <Button type="submit" disabled={form.formState.isSubmitting}>
+                Create Post
+              </Button>
+            </form>
+          </Form>
         </DialogContent>
       </Dialog>
     </div>
---
+++ lib/schemas.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const PostSchema = z.object({
+  id: z.string(),
+  fileUrl: z.string({ required_error: "File URL must be a valid URL" }).url(),
+  caption: z.string().optional(),
+});
+
+export const CreatePost = PostSchema.omit({ id: true });
+export const UpdatePost = PostSchema;
+export const DeletePost = PostSchema.pick({ id: true });
