f056b3e626344287a7828b7146f0ed538383e1e4
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-28 09:46:25 +0100

    Suspense Concept with Skeleton for loading animations.

--- app/dashboard/(home)/page.tsx
+++ app/dashboard/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Posts from "@/components/Posts";
+import { PostsSkeleton } from "@/components/Skeletons";
 import { Suspense } from "react";
 
 const DashboardPage = () => {
@@ -6,11 +7,7 @@ const DashboardPage = () => {
     <main className="flex w-full flex-grow">
       <div className="flex flex-col flex-1 gap-y-8 max-w-lg mx-auto pb-20">
         {/* using suspense */}
-        <Suspense
-        // fallback={
-        // <PostsSkeleton />
-        // }
-        >
+        <Suspense fallback={<PostsSkeleton />}>
           <Posts />
         </Suspense>
       </div>
