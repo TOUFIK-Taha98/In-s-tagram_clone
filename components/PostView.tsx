e2cf660b97ba2ef843a8c68d1e2a4d22ce3ab2b3
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2023-12-28 11:16:28 +0100

    Create View Post component in Next.js with NextAuth. & Create comment form with Next.js, CodeIgniter, Prisma, PlanetScale, and NextAuth. & Add post actions for user interaction, ensuring visibility on various screen sizes. Create loading spinner for enhanced user experience, utilize suspense boundaries.

--- components/PostView.tsx
+++ components/PostView.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-// import CommentForm from "@/components/CommentForm";
 import PostActions from "@/components/PostActions";
 import UserAvatar from "@/components/UserAvatar";
 import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
-// import { ScrollArea } from "@/components/ui/scroll-area";
 import useMount from "@/hooks/useMount";
 import { PostWithExtras } from "@/lib/definitions";
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useRef } from "react";
@@ -16,9 +13,7 @@ import { ScrollArea } from "./ui/scroll-area";
 import MiniPost from "./MiniPost";
 import Comment from "./Comment";
 import ViewPostButton from "./ViewPostButton";
-// import MiniPost from "./MiniPost";
-// import Comment from "./Comment";
-// import MiniPost from "./MiniPost";
+import CommentForm from "./CommentForm";
 
 interface PostViewProps {
   id: string;
